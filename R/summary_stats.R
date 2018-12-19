43f4ace5d68d0bd4d4899a103dd35f01ca1e9f58
Jeff Hollister <jeff.w.hollister@gmail.com>
2018-12-19 15:06:01 -0500

    some progress on the paper.

--- R/summary_stats.R
+++ R/summary_stats.R
@@ -1,10 +1,11 @@
 source(here::here("R/functions.R"))
 ww_all<-read_csv(here("data/ww_lake_trend_data.csv"))
-param_ranges <- ww_all %>%
+ww_param_ranges <- ww_all %>%
   filter(param == "chla" |
            param == "temp" |
            param == "total_n" |
            param == "total_p") %>%
+  mutate(param = factor(param,levels = c("temp", "total_n", "total_p", "chla"))) %>%
   group_by(param) %>%
   summarize(min = round(min(mn_measurement, na.rm = T), 2),
             percentile_25 = round(quantile(mn_measurement, probs = 0.25, na.rm = T), 2),
@@ -14,11 +15,42 @@ param_ranges <- ww_all %>%
             max = round(max(mn_measurement, na.rm = T), 2),
             sd = round(sd(mn_measurement, na.rm = T), 2)) 
 
-param_anomaly_ranges <- ww_all %>%
+ww_param_anomaly_ranges <- ww_all %>%
   filter(param == "chla" |
            param == "temp" |
            param == "total_n" |
            param == "total_p") %>%
+  mutate(param = factor(param,levels = c("temp", "total_n", "total_p", "chla"))) %>%
+  group_by(param) %>%
+  summarize(min = round(min(measurement_anmly, na.rm = T), 2),
+            percentile_25 = round(quantile(measurement_anmly, probs = 0.25, na.rm = T), 2),
+            mean = round(mean(measurement_anmly, na.rm = T), 2),
+            median = round(median(measurement_anmly, na.rm = T), 2),
+            percentile_75 = round(quantile(measurement_anmly, probs = 0.75, na.rm = T), 2),
+            max = round(max(measurement_anmly, na.rm = T), 2),
+            sd = round(sd(measurement_anmly, na.rm = T), 2))
+
+lagos_all <- read_csv(here("data/lagos_lake_trend_data.csv"))
+
+lagos_param_ranges <- lagos_all %>%
+  filter(param == "chla" |
+           param == "total_n" |
+           param == "total_p") %>%
+  mutate(param = factor(param,levels = c("total_n", "total_p", "chla"))) %>%
+  group_by(param) %>%
+  summarize(min = round(min(measurement, na.rm = T), 2),
+            percentile_25 = round(quantile(measurement, probs = 0.25, na.rm = T), 2),
+            mean = round(mean(measurement, na.rm = T), 2),
+            median = round(median(measurement, na.rm = T), 2),
+            percentile_75 = round(quantile(measurement, probs = 0.75, na.rm = T), 2),
+            max = round(max(measurement, na.rm = T), 2),
+            sd = round(sd(measurement, na.rm = T), 2)) 
+
+lagos_param_anomaly_ranges <- lagos_all %>%
+  filter(param == "chla" |
+           param == "total_n" |
+           param == "total_p") %>%
+  mutate(param = factor(param,levels = c("total_n", "total_p", "chla"))) %>%
   group_by(param) %>%
   summarize(min = round(min(measurement_anmly, na.rm = T), 2),
             percentile_25 = round(quantile(measurement_anmly, probs = 0.25, na.rm = T), 2),
