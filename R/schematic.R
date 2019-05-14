2324b48cc02f91aee2782e3d39ac437fdade5586
Jeff Hollister <jeff.w.hollister@gmail.com>
2019-05-14 08:58:36 -0400

    fixed schematic all the way down

--- R/schematic.R
+++ R/schematic.R
@@ -1,10 +1,13 @@
 library(readr)
 library(dplyr)
-schematic <- read_csv(here::here("data/schematic.csv"))
+library(here)
+
+schematic <- read_csv(here("data/schematic.csv"))
 
 station_year_mean <- schematic %>% 
   group_by(station, year) %>%
   summarize(station_year_mean = mean(meas)) %>%
+  round(2) %>%
   ungroup()
 
 station_year_z <- station_year_mean %>%
@@ -12,10 +15,13 @@ station_year_z <- station_year_mean %>%
   mutate(station_year_z = scale(station_year_mean),
          long_term_mean = mean(station_year_mean),
          long_term_sd = sd(station_year_mean)) %>%
+  round(2) %>%
   ungroup()
   
 mean_z <- station_year_z %>%
   group_by(year) %>%
   summarize(mean_z = mean(station_year_z),
             sd_z = sd(station_year_z),
-            n = n())
+            n = n()) %>%
+  round(2) %>%
+  ungroup()
